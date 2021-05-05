3f3911c81ccf1d1c3d8625807c0b26c4e5436682
danielboots <you@example.com>
2021-05-05 16:26:15 +0100

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f7263e2d8d3ee8caf9703765e17df99d'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '973c56b0-0da2-4ae8-8874-d608fc1cc385'
+};
